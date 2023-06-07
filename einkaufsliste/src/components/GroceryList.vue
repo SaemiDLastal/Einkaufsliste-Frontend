c995587cca09c395e9cb3496722b609953fbdefa
SaemiDLastal <104317393+SaemiDLastal@users.noreply.github.com>
2023-06-07 18:42:06 +0200

    Hello World

---
+++ einkaufsliste/src/components/GroceryList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="GroceryList">
+    <h1>This is a GroceryList page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroceryList',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
---
+++ einkaufsliste/src/views/GroceryListView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+import GroceryList from "../components/GroceryList.vue";
+
+export default {
+  name: 'GroceryListView',
+  components: {
+    GroceryList
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
