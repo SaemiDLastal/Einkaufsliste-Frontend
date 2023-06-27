20f4dd007462e4563a9b5d4ffdd9c139bb0a2886
SaemiDLastal <104317393+SaemiDLastal@users.noreply.github.com>
2023-06-27 10:58:15 +0200

    Frontend gestalten

--- einkaufsliste/src/views/GroceryListView.vue
+++ einkaufsliste/src/views/GroceryListView.vue
@@ -1,28 +1,76 @@
 <template>
-  <div class="grocery-list-app">
-    <h1>Grocery Lists</h1>
-    <div v-for="groceryList in groceryLists" :key="groceryList.id" class="grocery-list">
-      <h2>{{ groceryList.title }}</h2>
-      <ul>
-        <li v-for="item in groceryList.itemList" :key="item.id" :class="{ 'item-checked': item.checked }">
-          {{ item.name }}
-          <button @click="updateItemStatus(groceryList, item)">
-            {{ item.checked ? 'Uncheck' : 'Check' }}
-          </button>
-          <button @click="removeItem(groceryList, item)">Remove</button>
-        </li>
-      </ul>
-      <div>
-        <input type="text" v-model="newItemName" placeholder="Item name" />
-        <input type="text" v-model="newItemCategory" placeholder="Item category" />
-        <button @click="addItem(groceryList)">Add Item</button>
+  <div class="container">
+    <h1>Grocery List</h1>
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Create Grocery List</h2>
+        <form @submit.prevent="createGroceryList">
+          <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" class="form-control" v-model="newGroceryList.title" required>
+          </div>
+          <button type="submit" class="btn btn-primary">Create</button>
+        </form>
       </div>
-      <button @click="deleteGroceryList(groceryList)">Delete Grocery List</button>
+      <div class="col-md-8">
+        <h2>Grocery Lists</h2>
+        <table class="table">
+          <thead>
+          <tr>
+            <th>Title</th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="groceryList in groceryLists" :key="groceryList.id">
+            <td>{{ groceryList.title }}</td>
+            <td>
+              <button @click="showGroceryList(groceryList.id)" class="btn btn-primary">Show</button>
+              <button @click="deleteGroceryList(groceryList.id)" class="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div>
-      <h2>Create New Grocery List</h2>
-      <input type="text" v-model="newGroceryListTitle" placeholder="Grocery list title" />
-      <button @click="createGroceryList">Create Grocery List</button>
+    <div v-if="selectedGroceryList">
+      <h2>{{ selectedGroceryList.title }}</h2>
+      <table class="table">
+        <thead>
+        <tr>
+          <th>Item</th>
+          <th>Category</th>
+          <th>Quantity</th>
+          <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item) in selectedGroceryList.itemList" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{item.category}}</td>
+          <td>{{item.quantity}}</td>
+          <td>
+            <button @click="deleteItem(item.id)" class="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <h3>Add Item</h3>
+      <form @submit.prevent="addItem">
+        <div class="form-group">
+          <label for="itemName">Name:</label>
+          <input type="text" class="form-control" v-model="newItem.name" required>
+        </div>
+        <div class="form-group">
+          <label for="itemName">Category:</label>
+          <input type="text" class="form-control" v-model="newItem.category" required>
+        </div>
+        <div class="form-group">
+          <label for="itemQuantity">Quantity:</label>
+          <input type="number" class="form-control" v-model="newItem.quantity" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Add</button>
+      </form>
     </div>
   </div>
 </template>
@@ -32,9 +80,17 @@ export default {
   data() {
     return {
       groceryLists: [],
-      newGroceryListTitle: '',
-      newItemName: '',
-      newItemCategory: ''
+      selectedGroceryList: null,
+      newGroceryList: {
+        title: '',
+        itemList: []
+      },
+      newItem: {
+        name: '',
+        category: '',
+        quantity: 0
+      },
+
     };
   },
   mounted() {
@@ -42,99 +98,96 @@ export default {
   },
   methods: {
     fetchGroceryLists() {
-      fetch('/api/groceryList')
+      fetch('http://localhost:8080/api/groceryList')
           .then(response => response.json())
           .then(data => {
-            this.groceryLists= data;
+            this.groceryLists = data;
           })
           .catch(error => {
-            console.error('Error fetching grocery lists:', error);
+            console.error('Error:', error);
           });
     },
     createGroceryList() {
-      const newGroceryList = {
-        title: this.newGroceryListTitle,
-        itemList: []
-      };
-
-      fetch('/api/groceryList', {
+      fetch('http://localhost:8080/api/groceryList', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(newGroceryList)
+        body: JSON.stringify(this.newGroceryList)
       })
           .then(response => response.json())
           .then(data => {
             this.groceryLists.push(data);
-            this.newGroceryListTitle = '';
+            this.newGroceryList.title = '';
+            this.newGroceryList.itemList = [];
           })
           .catch(error => {
-            console.error('Error creating grocery list:', error);
+            console.error('Error:', error);
           });
     },
-    addItem(groceryList) {
-      const newItem = {
-        name: this.newItemName,
-        category: this.newItemCategory,
-        checked: false
-      };
-
-      fetch(`/api/groceryList/${groceryList.id}/item`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newItem)
-      })
+    showGroceryList(id) {
+      fetch(`http://localhost:8080/api/groceryList/${id}`)
           .then(response => response.json())
           .then(data => {
-            groceryList.itemList.push(data);
-            this.newItemName = '';
-            this.newItemCategory = '';
+            console.log(data);
+            this.selectedGroceryList = data;
           })
           .catch(error => {
-            console.error('Error adding item:', error);
+            console.error('Error:', error);
           });
     },
-    removeItem(groceryList, item) {
-      fetch(`/api/groceryList/${groceryList.id}/item`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(item)
-      })
-          .then(() => {
-            const index = groceryList.itemList.findIndex(i => i.id === item.id);
-            groceryList.itemList.splice(index, 1);
-          })
-          .catch(error => {
-            console.error('Error removing item:', error);
-          });
-    },
-    updateItemStatus(groceryList, item) {
-      fetch(`/api/groceryList/${groceryList.id}/item/${item.id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(item)
-      })
-          .catch(error => {
-            console.error('Error updating item status:', error);
-          });
-    },
-    deleteGroceryList(groceryList) {
-      fetch(`/api/groceryList/${groceryList.id}`, {
+    deleteGroceryList(id) {
+      fetch(`http://localhost:8080/api/groceryList/${id}`, {
         method: 'DELETE'
       })
           .then(() => {
-            const index = this.groceryLists.findIndex(list => list.id === groceryList.id);
-            this.groceryLists.splice(index, 1);
+            this.groceryLists = this.groceryLists.filter(list => list.id !== id);
+            this.selectedGroceryList = null;
           })
           .catch(error => {
-            console.error('Error deleting grocery list:', error);
+            console.error('Error:', error);
+          });
+    },
+    addItem() {
+      fetch(`http://localhost:8080/api/groceryList/${this.selectedGroceryList.id}/item`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.newItem)
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            this.selectedGroceryList.itemList= data;
+            this.newItem.name = '';
+            this.newItem.category = '';
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+    },
+    updateItem(item) {
+      fetch(`http://localhost:8080/api/item/${item.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+      })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+    },
+    deleteItem(item,id) {
+      fetch(`http://localhost:8080/api/groceryList/${this.selectedGroceryList.id}/item/${item.id}`, {
+        method: 'DELETE'
+      })
+          .then(() => {
+            this.selectedGroceryList.item = this.selectedGroceryList.item.filter(item => item.id !== id);
+          })
+          .catch(error => {
+            console.error('Error:', error);
           });
     }
   }
@@ -142,11 +195,7 @@ export default {
 </script>
 
 <style scoped>
-.grocery-list {
-  margin-bottom: 20px;
-}
-
-.item-checked {
-  text-decoration: line-through;
+.container {
+  margin-top: 40px;
 }
 </style>
